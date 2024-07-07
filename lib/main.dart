e63a10d23b83ace70de1fc2f218a6801e5ea6090
ndwMango <146925387+ndwMango@users.noreply.github.com>
2024-07-08 00:05:48 +0900

    first

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Container(),
+      ),
+    );
+  }
+}
